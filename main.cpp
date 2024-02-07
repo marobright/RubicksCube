7146c335eeeeccdbc2c38553c88a3a14e870864b
[Matthew Bright] <[marobright@gmail.com]>
2024-02-07 22:36:38 +0000

    random tinkering

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ int main()
 
 	Cube c;
     c.faces[0].printSide();
+	c.faces[1].printSide();
+	c.faces[2].printSide();
+	c.faces[3].printSide();
+	c.faces[4].printSide();
+	c.faces[5].printSide();
 	return 0;
 }
 
